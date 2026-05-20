988e14e4cf0e24ca29666c1b789b357774eaca67
janithachathuni <janithachathuni@gmail.com>
2026-05-20 17:01:50 +0530

    created springboot project

---
+++ src/test/java/com/example/kurullo/KurulloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kurullo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurulloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
