315e2bd4b38de4591e7edf817de6be3a9367a62e
janithachathuni <janithachathuni@gmail.com>
2026-06-26 13:55:25 +0530

    cleaning up

--- src/main/java/com/example/kurullo/security/SecurityConfig.java
+++ src/main/java/com/example/kurullo/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.kurullo.security;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +10,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
